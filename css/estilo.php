ae7546aa88a846e63d18171c63a44fe4bb4925fd
ricardoxd <chessdj@hotmail.com>
2025-06-04 10:06:09 -0400

    Create estilo.php

---
+++ css/estilo.php
@@ -0,0 +1,401 @@
+
+
+body {
+	background-color:#787878;
+
+}
+main {
+	background-color:#ffffff;
+
+}
+nav {
+
+}
+header {
+
+}
+section {
+	background-color:#cfcfcf;
+
+}
+aside {
+
+}
+footer {
+
+}
+img {
+
+}
+a {
+
+}
+hr {
+
+}
+article {
+
+}
+li {
+
+}
+div {
+
+}
+p {
+
+}
+fieldset {
+	background-color:#cfcfcf;
+
+}
+legend {
+	background-color:#797979;
+	color:#ffffff;
+
+}
+td {
+
+}
+th {
+	background-color:#a2a2a2;
+
+}
+form {
+
+}
+h1 {
+	color:#505050;
+
+}
+h2 {
+	color:#888383;
+
+}
+h3 {
+	color:#c5c5c5;
+
+}
+label {
+	margin:20px;
+
+}
+input {
+
+}
+select {
+
+}
+option {
+
+}
+textarea {
+
+}
+progress {
+
+}
+ul {
+	background-color:#898989;
+	color:#ffffff;
+
+}
+button {
+	background-color:#023eca;
+	height:30px;
+	padding:5px;
+	margin:20px;
+	color:#ffffff;
+
+}
+#header_163 {
+	background-color:#757575;
+	color:#ffffff;
+	width:90%;
+	font-size:20px;
+	margin-right:10%;
+	margin-left:10%;
+
+}
+#section_164 {
+	background-color:;
+	margin-left:10%;
+	margin-right:10%;
+
+}
+#div_165 {
+	margin-left:10%;
+	margin-right:10%;
+
+}
+#header_163:hover {
+	background-color:#240000;
+
+}
+#header_163:focus {
+	background-color:#1130a6;
+
+}
+#header_163:target {
+	background-color:#64bb4c;
+
+}
+.contenido {
+
+}
+.escritorio {
+
+}
+.prueba {
+	color:#e0dc68;
+
+}
+.tema {
+
+}
+
+
+@media all and (max-width: 1024px), 
+only screen and (min-device-width : 481px) and (max-device-width : 1024px),
+only screen and (-webkit-min-device-pixel-ratio: 2) and (max-width: 1024px),
+ only screen and (min--moz-device-pixel-ratio: 2) and (max-width: 1024px),
+  only screen and (-o-min-device-pixel-ratio: 2/1) and (max-width: 1024px),
+   only screen and (min-device-pixel-ratio: 2) and (max-width: 1024px), 
+   only screen and (min-resolution: 192dpi) and (max-width: 1024px), 
+   only screen and (min-resolution: 2dppx) and (max-width: 1024px) {
+
+	body {
+
+}
+main {
+
+}
+nav {
+
+}
+header {
+
+}
+section {
+
+}
+aside {
+
+}
+footer {
+
+}
+img {
+
+}
+a {
+
+}
+hr {
+
+}
+article {
+
+}
+li {
+
+}
+div {
+
+}
+p {
+
+}
+fieldset {
+
+}
+legend {
+
+}
+td {
+
+}
+th {
+
+}
+form {
+
+}
+h1 {
+
+}
+h2 {
+
+}
+h3 {
+
+}
+label {
+
+}
+input {
+
+}
+select {
+
+}
+option {
+
+}
+textarea {
+
+}
+progress {
+
+}
+ul {
+
+}
+button {
+
+}
+#header_163 {
+	background-color:#d7d7d7;
+
+}
+#header_163:hover {
+	background-color:#d28282;
+
+}
+#header_163:focus {
+	background-color:#e79797;
+
+}
+#header_163:target {
+	background-color:#a9bfe6;
+
+}
+.contenido {
+
+}
+.escritorio {
+
+}
+.prueba {
+	font-size:30px;
+
+}
+.tema {
+
+}
+ }
+
+
+
+@media all and (max-width: 480px), 
+only screen and (min-device-width : 320px) and (max-device-width : 480px),
+only screen and (-webkit-min-device-pixel-ratio: 2) and (max-width: 480px),
+ only screen and (min--moz-device-pixel-ratio: 2) and (max-width: 480px),
+  only screen and (-o-min-device-pixel-ratio: 2/1) and (max-width: 480px),
+   only screen and (min-device-pixel-ratio: 2) and (max-width: 480px), 
+   only screen and (min-resolution: 192dpi) and (max-width: 480px), 
+   only screen and (min-resolution: 2dppx) and (max-width: 480px) {
+
+	body {
+
+}
+main {
+
+}
+nav {
+
+}
+header {
+
+}
+section {
+
+}
+aside {
+
+}
+footer {
+
+}
+img {
+
+}
+a {
+
+}
+hr {
+
+}
+article {
+
+}
+li {
+
+}
+div {
+
+}
+p {
+
+}
+fieldset {
+
+}
+legend {
+
+}
+td {
+
+}
+th {
+
+}
+form {
+
+}
+h1 {
+
+}
+h2 {
+
+}
+h3 {
+
+}
+label {
+
+}
+input {
+
+}
+select {
+
+}
+option {
+
+}
+textarea {
+
+}
+progress {
+
+}
+ul {
+
+}
+button {
+
+}
+.contenido {
+
+}
+.escritorio {
+
+}
+.prueba {
+
+}
+.tema {
+
+}
+ }
